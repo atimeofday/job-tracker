58585636737bfbe8dac4cb730e38a0a935c83871
Ian Off <atimeofday@gmail.com>
2025-02-15 13:55:50 -0600

    Add Handshake to supported job board sources

--- jobtracker.py
+++ jobtracker.py
@@ -51,6 +51,7 @@ def interact():
         'w':    'Wellfound',
         'n':    'LinkedIn',
         'b':    'BuiltIn',
+        'h':    'Handshake',
         'c':    'Company',
         'r':    'Referral',
     }
